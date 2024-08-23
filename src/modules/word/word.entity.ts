396a311e83bb9f48471d891bfe4ec6968bc1c7a2
Aviv BS <avivbs2468@gmail.com>
2024-08-23 22:32:17 +0300

    remove: deleted word module

--- src/modules/word/word.entity.ts
+++
@@ -1,7 +0,0 @@
-import { Entity, PrimaryColumn } from 'typeorm';
-
-@Entity({ name: 'words' })
-export class Word {
-  @PrimaryColumn({ type: 'varchar', length: 30 })
-  value: string;
-}
--- src/modules/wordGroup/wordGroup.entity.ts
+++ src/modules/wordGroup/wordGroup.entity.ts
@@ -1,10 +1,9 @@
 import { Entity, PrimaryColumn } from 'typeorm';
-import { Word } from '../word/word.entity';
 
 @Entity({ name: 'word_group' })
 export class WordGroup {
   @PrimaryColumn({ type: 'varchar', length: 30 })
-  word: Word;
+  word: string;
 
   @PrimaryColumn({ type: 'varchar', length: 50, name: 'group_name' })
   groupName: string;
