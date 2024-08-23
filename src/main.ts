a19bf0217b346d5c74744875c71924c1d1d6fa28
Aviv BS <avivbs2468@gmail.com>
2024-08-23 17:31:54 +0300

    feat: added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
@@ -6,6 +7,17 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Songs Finder')
+  .setDescription('The songs finder API description')
+  .setVersion('1.0')
+  .addTag('songs')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+
   console.log('Start to listen in port: ', app.get<ConfigService>(ConfigService).get('port'))
   await app.listen(app.get<ConfigService>(ConfigService).get('port'));
 }
